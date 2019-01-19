f7aaf0baf6512b727c9896afc50b41bc65931180
eryktrzeciakiewicz <eryk.trzeciakiewicz@gmail.com>
2019-01-19 10:22:17 +0100

    Added the RPNCalculator class which is the facade between the client and the algorithm

---
+++ expression-calculator/RPNCalculator.py
@@ -0,0 +1,16 @@
+from input_parser import InputParser
+from validator import Validator
+from rpn_processor import RPNProcessor
+
+class RPNCalculator:
+    
+    def __init__(self):
+        self.parser = InputParser()
+        self.processor = RPNProcessor()
+
+    def to_rpn(self, expression):
+        return self.parser.to_rpn(expression)
+    
+    def calculate(self, expressioin):
+        rpn = self.parser.to_rpn(expressioin)
+        return self.processor.evaluate_rpn(rpn)
